de17485726c53f11f06d5f839ce9c3c2b1b9c8be
Sanskriti-01 <sanskritirastogi118@gmail.com>
2026-09-11 23:03:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (18.9%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans = new ArrayList<>();
+        long res = 1;
+
+        for(int i = 0; i <= rowIndex; i++){
+            ans.add((int) res);
+
+            res = res * (rowIndex - i) ;
+            res=res/(i+1);
+        }
+
+        return ans;
+    }
+}
