03cc898f123f3c5ce06c75a3fe01fa518787b529
Sanskriti-01 <sanskritirastogi118@gmail.com>
2026-09-22 23:25:21 +0530

    Time: 1 ms (92.12%), Space: 45.7 MB (8.59%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sum=0;
+        int sum2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=9){
+                sum=sum+nums[i];
+            }
+            else{
+                sum2=sum2+nums[i];
+            }
+            
+        }
+        if(sum==sum2)
+        return false;
+        else
+        return true;
+    }
+}
